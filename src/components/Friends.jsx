6d3d1cb11e8fd3bd957b5ceb0de2eb313477a2fb
sujan <dev.mern.sujan@gmail.com>
2022-10-11 14:56:03 +0600

    copy kore baki gulo apatoto bosailam

---
+++ src/components/Friends.jsx
@@ -0,0 +1,173 @@
+import React from "react";
+
+const Friends = () => {
+  return (
+    <div className="rounded-2xl p-10 h-[451px] overflow-y-scroll shadow-md">
+      <h1 className="font-nunito font-bold text-lg">Friends</h1>
+
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-4 border-b pb-2.5 items-center last:border-0">
+        <div>
+          <img src="images/groupimg.png" className="w-16 h-16 rounded-[50%]" />
+        </div>
+        <div>
+          <h1 className="font-nunito font-bold text-base">Raghav</h1>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Dinner?
+          </p>
+        </div>
+        <div>
+          <p className="font-nunito font-semibold text-sm opacity-60">
+            Today, 8:56pm
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Friends;
--- src/page/home/index.js
+++ src/page/home/index.js
@@ -2,19 +2,31 @@ import React from "react";
 import Sidebar from "../../components/Sidebar";
 import Search from "../../components/Search";
 import Group from "../../components/Group";
+import Friends from "../../components/Friends";
+import FriendRequist from "../../components/FriendRequist";
+import MyGroups from "../../components/MyGroups";
+import UserList from "../../components/UserList";
+import BlockedUsers from "../../components/BlockedUsers";
 
 const Home = () => {
   return (
     <div className="flex justify-between">
-      <div className="max-w-[186px]">
+      <div className="w-[186px]">
         <Sidebar active="home" />
       </div>
-      <div className="max-w-[427px]">
+      <div className="w-[540px]">
         <Search />
         <Group />
+        <FriendRequist />
       </div>
-      <div className="max-w-[344px]">dkfjdk</div>
-      <div className="max-w-[344px]">dkfd</div>
+      <div className="w-[540px]">
+        <Friends />
+        <MyGroups />
+      </div>
+      <div className="w-[540px]">
+        <UserList />
+        <BlockedUsers />
+      </div>
     </div>
   );
 };

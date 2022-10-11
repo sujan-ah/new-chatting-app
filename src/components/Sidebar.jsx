9974db3c7e0c8dd72188274505d08920533c83b4
sujan <dev.mern.sujan@gmail.com>
2022-10-11 13:13:48 +0600

    group design done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,19 +5,45 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import { AiOutlineSetting } from "react-icons/ai";
 import { HiOutlineLogout } from "react-icons/hi";
 
-const Sidebar = () => {
+const Sidebar = ({ active }) => {
   return (
-    <div className="bg-primary px-11 py-10 h-screen overflow-y-hidden		">
+    <div className="bg-primary px-11 py-10 h-screen overflow-x-hidden overflow-y-hidden		">
       <img
         src="images/profileimg.png"
         className="w-[100px] h-[100px] rounded-[50%]"
       />
-      <div className="flex flex-col items-center gap-y-20 mt-20 ">
-        <MdOutlineHome className="text-5xl text-white " />
-        <TbMessageCircle className="text-5xl text-white" />
+      <div className="flex flex-col items-center gap-y-16 mt-20 mb-20">
+        <div
+          className={`${
+            active == "home" &&
+            "relative z-10 after:absolute after:top-0 after:left-0 after:content-[''] after:bg-white after:w-[243%] after:h-full  after:z-[-1] px-11 py-5 after:rounded-3xl before:absolute before:top-0 before:right-[-34px] before:content-[''] before:bg-primary before:w-[15%] before:h-full before:rounded-3xl before:drop-shadow-2xl"
+          }`}
+        >
+          <MdOutlineHome
+            className={`${
+              active == "home" ? "text-5xl text-black" : "text-5xl text-white"
+            }`}
+          />
+        </div>
+
+        <div
+          className={`${
+            active == "message" &&
+            "relative z-10 after:absolute after:top-0 after:left-0 after:content-[''] after:bg-white after:w-[243%] after:h-full  after:z-[-1] px-11 py-5 after:rounded-3xl before:absolute before:top-0 before:right-[-34px] before:content-[''] before:bg-primary before:w-[15%] before:h-full before:rounded-3xl before:drop-shadow-2xl"
+          }`}
+        >
+          <TbMessageCircle
+            className={`${
+              active == "message"
+                ? "text-5xl text-black"
+                : "text-5xl text-white"
+            }`}
+          />
+        </div>
+
         <IoMdNotificationsOutline className="text-5xl text-white" />
         <AiOutlineSetting className="text-5xl text-white " />
-        <HiOutlineLogout className="text-5xl text-white mt-36" />
+        <HiOutlineLogout className="text-5xl text-white mt-32" />
       </div>
     </div>
   );
--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar";
+import Search from "../../components/Search";
+import Group from "../../components/Group";
 
 const Home = () => {
   return (
-    <div className="flex">
+    <div className="flex justify-between">
       <div className="max-w-[186px]">
-        <Sidebar />
+        <Sidebar active="home" />
       </div>
-      <div className="max-w-[427px]"></div>
-      <div className="max-w-[344px]"></div>
-      <div className="max-w-[344px]"></div>
+      <div className="max-w-[427px]">
+        <Search />
+        <Group />
+      </div>
+      <div className="max-w-[344px]">dkfjdk</div>
+      <div className="max-w-[344px]">dkfd</div>
     </div>
   );
 };

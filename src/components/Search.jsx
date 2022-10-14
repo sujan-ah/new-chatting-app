f768cab308a37fc3dc2d0f48aec27c7ef01b8888
sujan <dev.mern.sujan@gmail.com>
2022-10-14 17:16:13 +0600

    resposive pray done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,13 @@ import { FiSearch } from "react-icons/fi";
 
 const search = () => {
   return (
-    <div className="relative">
+    <div className="relative ">
       <div className="flex">
         <FiSearch className="absolute top-5 ml-6 text-2xl mt-[-7px]" />
         <input
           type="search"
           placeholder="search"
-          className=" border border-solid shadow-md rounded-2xl w-[427px] px-20 py-3 font-nunito font-semibold text-base"
+          className=" border border-solid shadow-md rounded-2xl w-full xl:w-[427px] px-20 py-3 font-nunito font-semibold text-base"
         />
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,21 +7,23 @@ import { HiOutlineLogout } from "react-icons/hi";
 
 const Sidebar = ({ active }) => {
   return (
-    <div className="bg-primary px-11 py-10 h-screen overflow-x-hidden overflow-y-hidden		">
+    <div className="flex justify-center xl:block bg-primary px-5 py-5 xl:px-11 xl:py-10 xl:h-screen overflow-x-hidden overflow-y-hidden	fixed bottom-0 xl:static w-full ml-[-12px] xl:ml-0">
       <img
         src="images/profileimg.png"
-        className="w-[100px] h-[100px] rounded-[50%]"
+        className="w-[60px] xl:w-[100px] xl:h-[100px] rounded-[50%]"
       />
-      <div className="flex flex-col items-center gap-y-16 mt-20 mb-20">
+      <div className="flex xl:flex-col items-center gap-x-5 xl:gap-x-0 gap-y-16  xl:mt-20 xl:mb-20 ml-5 xl:ml-0">
         <div
           className={`${
             active == "home" &&
-            "relative z-10 after:absolute after:top-0 after:left-0 after:content-[''] after:bg-white after:w-[243%] after:h-full  after:z-[-1] px-11 py-5 after:rounded-3xl before:absolute before:top-0 before:right-[-34px] before:content-[''] before:bg-primary before:w-[15%] before:h-full before:rounded-3xl before:drop-shadow-2xl"
+            "relative z-10 after:absolute after:top-0 after:left-0 after:content-[''] after:bg-white xl:after:w-[243%] after:h-full  after:z-[-1] xl:px-11 xl:py-5 after:rounded-3xl before:absolute before:top-0 before:right-[-34px] before:content-[''] xl:before:bg-primary xl:before:w-[15%] before:h-full before:rounded-3xl before:drop-shadow-2xl"
           }`}
         >
           <MdOutlineHome
             className={`${
-              active == "home" ? "text-5xl text-black" : "text-5xl text-white"
+              active == "home"
+                ? "text-3xl xl:text-5xl text-white xl:text-primary"
+                : "text-3xl xl:text-5xl text-white"
             }`}
           />
         </div>
@@ -35,15 +37,15 @@ const Sidebar = ({ active }) => {
           <TbMessageCircle
             className={`${
               active == "message"
-                ? "text-5xl text-black"
-                : "text-5xl text-white"
+                ? "text-3xl xl:text-5xl text-black"
+                : "text-3xl  xl:text-5xl text-white"
             }`}
           />
         </div>
 
-        <IoMdNotificationsOutline className="text-5xl text-white" />
-        <AiOutlineSetting className="text-5xl text-white " />
-        <HiOutlineLogout className="text-5xl text-white mt-32" />
+        <IoMdNotificationsOutline className="text-3xl xl:text-5xl text-white" />
+        <AiOutlineSetting className="text-3xl xl:text-5xl text-white " />
+        <HiOutlineLogout className="text-3xl xl:text-5xl text-white xl:mt-32" />
       </div>
     </div>
   );

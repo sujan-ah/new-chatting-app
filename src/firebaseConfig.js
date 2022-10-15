5b31f9e5c5d83b427797094ebb412cbeedfb55c4
sujan <dev.mern.sujan@gmail.com>
2022-10-15 13:54:06 +0600

    email send in firebase database

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2JI0XQcpV9owXAmXWCb79ftNzW7Ea01Y",
+  authDomain: "chatting-app-49195.firebaseapp.com",
+  projectId: "chatting-app-49195",
+  storageBucket: "chatting-app-49195.appspot.com",
+  messagingSenderId: "748954528400",
+  appId: "1:748954528400:web:806046020390ed0bb4c615",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;

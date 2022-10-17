e72702edc172b747949b3899f76f510d90820676
sujan <dev.mern.sujan@gmail.com>
2022-10-17 17:11:15 +0600

    logout & user na thakle redirect login from home done

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect } from "react";
 import Sidebar from "../../components/Sidebar";
 import Search from "../../components/Search";
 import Group from "../../components/Group";
@@ -7,8 +7,20 @@ import FriendRequist from "../../components/FriendRequist";
 import MyGroups from "../../components/MyGroups";
 import UserList from "../../components/UserList";
 import BlockedUsers from "../../components/BlockedUsers";
+import { getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  console.log(auth.currentUser);
+
+  useEffect(() => {
+    if (!auth.currentUser) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <div className="xl:flex justify-between p-2.5 xl:p-0">
       <div className="xl:w-[186px] ">

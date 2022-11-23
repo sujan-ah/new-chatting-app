408a387811b0f0d7a3a8eb55bc8072bc3f8acf3d
sujan <dev.mern.sujan@gmail.com>
2022-11-23 15:05:25 +0600

    react redux set up korlam & devtools e data pelam

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from "./App";
 import firebaseConfig from "./firebaseConfig";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
 

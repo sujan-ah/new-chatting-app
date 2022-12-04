a17fded04e852ff65d652e995d410093d190c081
sujan <dev.mern.sujan@gmail.com>
2022-12-05 00:31:10 +0600

    notification show done & delete notification length after click done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Registration from "./page/registration/index";
 import Login from "./page/login/index";
 import Home from "./page/home/index";
 import Message from "./page/message";
+import Notification from "./page/notification";
 import ForgotPassword from "./page/forgot password";
 import Imgupload from "./components/Imgupload";
 
@@ -15,6 +16,7 @@ function App() {
       <Route path="/registration" element={<Registration />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/message" element={<Message />}></Route>
+      <Route path="/notification" element={<Notification />}></Route>
       <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
       <Route path="/imgupload" element={<Imgupload />}></Route>
     </Routes>
